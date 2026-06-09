ce0ec2e38bd259f168f165ed6eaeda25224408dd
Ouiame19 <o.tayibi9607@uca.ac.ma>
2026-06-09 23:09:39 +0100

    Test JaCoCo coverage report

--- src/main/java/com/example/demo/dto/WorkingConfigDTO.java
+++ src/main/java/com/example/demo/dto/WorkingConfigDTO.java
@@ -1,5 +1,6 @@
 package com.example.demo.dto;
 
+import com.example.demo.entities.WorkingConfig;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
@@ -39,5 +40,23 @@ public class WorkingConfigDTO {
 
     public void setSlotDurationMinutes(int slotDurationMinutes) {
         this.slotDurationMinutes = slotDurationMinutes;
+    }
+
+    public static WorkingConfigDTO fromEntity(WorkingConfig entity) {
+        WorkingConfigDTO dto = new WorkingConfigDTO();
+        dto.setDayOfWeek(entity.getDayOfWeek());
+        dto.setStartTime(entity.getStartTime());
+        dto.setEndTime(entity.getEndTime());
+        dto.setSlotDurationMinutes(entity.getSlotDurationMinutes());
+        return dto;
+    }
+
+    public WorkingConfig toEntity() {
+        WorkingConfig config = new WorkingConfig();
+        config.setDayOfWeek(this.dayOfWeek);
+        config.setStartTime(this.startTime);
+        config.setEndTime(this.endTime);
+        config.setSlotDurationMinutes(this.slotDurationMinutes);
+        return config;
     }
 }
